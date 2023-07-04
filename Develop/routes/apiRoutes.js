e71d60e7734112a837b1afc0f7e4f6a18d85c797
KGBerry542 <74325710+KGBerry542@users.noreply.github.com>
2023-07-04 13:12:27 -0400

    adding apiRoutes

---
+++ Develop/routes/apiRoutes.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const fsUtils = require('../helpers/fsUtils');
+
+const router = express.Router();
+
+router.get('/notes', (req, res) => {
+  fsUtils.readFromFile('db/db.json')
+    .then(data => res.json(data))
+    .catch(err => res.status(500).json({ error: 'Failed to read notes data.' }));
+});
+
+router.post('/notes', (req, res) => {
+  fsUtils.readFromFile('db/db.json')
+    .then(data => {
+      const notes = JSON.parse(data);
+      const newNote = req.body;
+      notes.push(newNote);
+      return fsUtils.writeToFile('db/db.json', JSON.stringify(notes));
+    })
+    .then(() => res.json(req.body))
+    .catch(err => res.status(500).json({ error: 'Failed to save the note.' }));
+});
+
+module.exports = router;
